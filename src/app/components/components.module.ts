647f7e490160e48c807ba658141a1267b2eb4adf
Saúl Laínez <saullainez@hotmail.es>
2021-05-07 15:38:09 -0600

    Configuración del gráfico inicial

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,18 +3,23 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GraficoBarraHorizontalComponent } from './grafico-barra-horizontal/grafico-barra-horizontal.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 
 
 @NgModule({
   exports: [
     NavbarComponent,
-    NavbarComponent
+    GraficoBarraHorizontalComponent,
+    NgxChartsModule
   ],
   declarations: [NavbarComponent, GraficoBarraHorizontalComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BrowserAnimationsModule,
+    NgxChartsModule
   ]
 })
 export class ComponentsModule { }
--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css']
 })
-export class GraficoBarraHorizontalComponent implements OnInit {
+export class GraficoBarraHorizontalComponent {
 
-  constructor() { }
+  results: any[] = [
+    {
+      "name": "Juego 1",
+      "value": 20
+    },
+    {
+      "name": "Juego 2",
+      "value": 10
+    },
+    {
+      "name": "Juego 3",
+      "value": 40
+    }
+  ];
+  multi: any[];
 
-  ngOnInit(): void {
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Juegos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Votos';
+
+  colorScheme = 'nightLights';
+
+  constructor() {}
+
+  onSelect(event) {
+    console.log(event);
   }
-
 }
