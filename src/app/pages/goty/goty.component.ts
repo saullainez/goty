d5faa2eba9f423bd3ebfe2865c12358fa50dea4e
Saúl Laínez <saullainez@hotmail.es>
2021-05-12 08:04:02 -0600

    HTTP: GET
    Obtener los juegos

--- src/app/pages/goty/goty.component.ts
+++ src/app/pages/goty/goty.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../../interfaces/interfaces';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-goty',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GotyComponent implements OnInit {
 
-  constructor() { }
+  games: Game[] = [];
+
+  constructor(
+    private gameService: GameService
+  ) { }
 
   ngOnInit(): void {
+    this.gameService.getNominados()
+      .subscribe( resp => {
+        this.games = resp;
+      });
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'http://localhost:5000/firestore-grafica-42e5b/us-central1'
 };
 
 /*
