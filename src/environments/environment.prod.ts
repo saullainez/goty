9dfbae160572a49d048a7cdd5dae50ffd59df095
Saúl Laínez <saullainez@hotmail.es>
2021-05-13 09:11:14 -0600

    Update environments variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true,
   url: 'https://us-central1-firestore-grafica.cloudfunctions.net',
-  firebase: {
-    apiKey: "AIzaSyB6eukyWpvqneq8BtxYZaOmlPSGuC8WF5U",
-    authDomain: "firestore-grafica-42e5b.firebaseapp.com",
-    databaseURL: "https://firestore-grafica-42e5b-default-rtdb.firebaseio.com",
-    projectId: "firestore-grafica-42e5b",
-    storageBucket: "firestore-grafica-42e5b.appspot.com",
-    messagingSenderId: "57848431940",
-    appId: "1:57848431940:web:bb27d3270640b775764311"
-  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,7 @@
 export const environment = {
   production: false,
   url: 'http://localhost:5000/firestore-grafica-42e5b/us-central1',
-  firebase: {
-    apiKey: "AIzaSyB6eukyWpvqneq8BtxYZaOmlPSGuC8WF5U",
-    authDomain: "firestore-grafica-42e5b.firebaseapp.com",
-    databaseURL: "https://firestore-grafica-42e5b-default-rtdb.firebaseio.com",
-    projectId: "firestore-grafica-42e5b",
-    storageBucket: "firestore-grafica-42e5b.appspot.com",
-    messagingSenderId: "57848431940",
-    appId: "1:57848431940:web:bb27d3270640b775764311"
-  }
+
 };
 
 /*
